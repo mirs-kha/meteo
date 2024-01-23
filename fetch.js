6d8810befd1ed5a5725acbcd1510daef61872f36
mirs-kha <camir@live.fr>
2024-01-23 09:34:55 +0100

    test import

--- fetch.js
+++ fetch.js
@@ -1,4 +1,4 @@
-import { searchTown } from './searchTown.js';
+
 const ico = document.querySelector('.ico')
 const ville = document.querySelector('.ville')
 const humidite = document.querySelector('#humidite')
@@ -21,7 +21,25 @@ else {
     var villeActuelle = document.querySelector('#in').value;
 }
 
+async function searchTown() {
+    valid.addEventListener("click", async function () {
+        // Notez que vous devez réaffecter la valeur renvoyée à la variable villeActuelle
+        villeActuelle = await recupererForm();
+        console.log(villeActuelle);
+        // Vous pouvez appeler les fonctions qui dépendent de villeActuelle ici
+        singleWeather(villeActuelle);
+        singleWeatherInfos(villeActuelle);
+        singleWeatherAllDay(villeActuelle);
+        fetchWeatherMinMax(villeActuelle);
+    });
 
+    async function recupererForm() {
+        const getTown = document.querySelector('#in').value;
+        console.log(getTown);
+        // Renvoie la nouvelle valeur de villeActuelle
+        return getTown;
+    }
+}
 searchTown()
 async function singleWeather(villeActuelle) {
     try {
